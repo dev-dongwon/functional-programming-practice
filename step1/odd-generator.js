dca81713b67970f5e5d91884a8e5cebba4251ea3
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-11 16:43:55 +0900

    Feat : odd generator 구현
    infinity, limit, odd 제너레이터 합성

---
+++ step1/odd-generator.js
@@ -0,0 +1,35 @@
+// step1
+function *odd() {
+  yield 1;
+  yield 3;
+  yield 5;
+}
+
+// step2 - infinity 함수로 refactoring
+function *infinity(i = 0) {
+  while(true) yield i++;
+}
+
+function *odd() {
+  for (const a of infinity(1)) {
+    if (a % 2 === 1) yield a;
+  }
+}
+
+// step3 - limit 함수까지 더하면
+function *limit(l, iter) {
+  for (const a of iter) {
+    yield a;
+    if (a === l) return;
+  }
+}
+
+function *odds(l) {
+  for (const a of limit(l, infinity(1))) {
+    if (a % 2 ) yield a; 
+  }
+}
+
+for (const a of odds(40)) {
+  log(a);
+}
