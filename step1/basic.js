86b0cd6ff6b98035142f521ffcde7f4a87e2a35f
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-11 16:11:06 +0900

    Feat : 함수형 프로그래밍 기본
    고차함수, iterable protocol, generator

---
+++ step1/basic.js
@@ -0,0 +1,45 @@
+// 함수를 인자로 받아 실행하는 함수
+const log = console.log;
+
+const apply = f => f(1);
+const add2 = (number) => number + 2;
+log(apply(add2)) // 3
+
+
+const times = (f, n) => {
+  let i = -1;
+  while(++i < n) f(i);
+}
+times(log, 3); // 0, 1, 2
+
+
+const addMaker = a => b => a + b;
+const add10 = addMaker(10);
+log(add10); // b => a + b;
+log(add10(10)); // 20
+
+
+// 이터러블 프로토콜
+const arr = [1,2,3];
+log(arr[Symbol.iterator]); // ƒ values() { [native code] }
+log(arr[Symbol.iterator]()); // Array Iterator {}
+
+let iter = arr[Symbol.iterator]();
+log(iter.next()); // {value: 1, done: false}
+log(iter.next()); // {value: 2, done: false}
+log(iter.next()); // {value: 3, done: false}
+log(iter.next()); // {value: undefined, done: true}
+
+
+// 제너레이터 - 이터레이터이자 이터러블을 생성하는 함수
+function *gen() {
+  yield 1;
+  if (false) yield 2;
+  yield 3;
+}
+
+iter = gen();
+log(iter); // gen {<suspended>}
+log(iter.next()); // {value: 1, done: false}
+log(iter.next()); // {value: 3, done: false}
+log(iter.next()); // {value: undefined, done: true}
