9fe911c722a4cce9135c6024ec5a06784285d59a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-23 19:01:23 +0900

    Feat : curry 함수 적용
    curry 함수 map, filter, reduce에 적용

--- fs.js
+++ fs.js
@@ -1,22 +1,24 @@
 const log = console.log;
 
-const map = (f, iter) => {
+const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const map = curry((f, iter) => {
   const res = [];
   for (const a of iter) {
     res.push(f(a));
   }
   return res;
-}
+});
 
-const filter = (f, iter) => {
+const filter = curry((f, iter) => {
   const res = [];
   for(const a of iter) {
     if(f(a)) res.push(a);
   }
   return res;
-}
+});
 
-const reduce = (f, acc, iter) => {
+const reduce = curry((f, acc, iter) => {
   // acc가 없이 (f, iter) 이렇게만 들어간 경우
   if (!iter) {
     iter = acc[Symbol.iterator]();
@@ -27,9 +29,8 @@ const reduce = (f, acc, iter) => {
     acc = f(acc, a);
   }
   return acc;
-}
+});
 
 const go = (...args) => reduce((acc, fn) => fn(acc), args);
 
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
-
